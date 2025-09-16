11a90fec5355dfe93b9e14348f562480a055cfe1
Apotatoflew <76245607+Apotatoflew@users.noreply.github.com>
2025-09-16 20:03:37 +0300

    Update main.c
    Vaihdoin pois väärän (toki toimivan) version koodista

--- blinky_1/src/main.c
+++ blinky_1/src/main.c
@@ -7,13 +7,18 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
-// Add semaphore
-struct k_sem sem_red;
-struct k_sem sem_green;
-struct k_sem sem_yellow;
+
+//Semaphores testi
+
+//struct k_sem sem_red;
+//struct k_sem sem_green;
+//struct k_sem sem_yellow;
+
+int valo = 0;
+int entinen = 0;
+bool seis = false;
 
 
-// Task declarations
 void task1(void *, void *, void*);
 void task2(void *, void *, void*);
 void task3(void *, void *, void*);
@@ -29,11 +34,12 @@ static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 //static const struct gpio_dt_spec blue = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 
+
 int init_led() {
     int ret = gpio_pin_configure_dt(&red, GPIO_OUTPUT_ACTIVE);
     //int ret2 = gpio_pin_configure_dt(&blue, GPIO_OUTPUT_ACTIVE);
     int ret3 = gpio_pin_configure_dt(&green, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0 || /*/ret2 < 0*/ ret3 < 0) {
+    if (ret < 0 ||  ret3 < 0) {
         printk("Error: Led configure failed\n");
         return -1;
     }
@@ -43,16 +49,17 @@ int init_led() {
 int main(void) {
     init_led();
     printk("LEDs initialized\n");
-    k_sem_init(&sem_red, 1, 1);    // Start with red allowed
-    k_sem_init(&sem_yellow, 0, 1);   // Blue waits
-    k_sem_init(&sem_green, 0, 1);  // Green waits
+    //k_sem_init(&sem_red, 1, 1);    // Start with red allowed
+    //k_sem_init(&sem_yellow, 0, 1);   // Blue waits
+    //k_sem_init(&sem_green, 0, 1);  // Green waits
     return 0;
 }
 
 // Task1 function (Red)
 void task1(void *, void *, void*) {
+if (valo == 0) {   
     while (true) {
-        k_sem_take(&sem_red, K_FOREVER);
+        //k_sem_take(&sem_red, K_FOREVER);
         gpio_pin_set_dt(&red, 1);
         //gpio_pin_set_dt(&blue, 0);
         gpio_pin_set_dt(&green, 0);
@@ -61,14 +68,18 @@ void task1(void *, void *, void*) {
         gpio_pin_set_dt(&red, 0);
         printk("Red off\n");
         k_msleep(500);
-        k_sem_give(&sem_yellow); // Next: blue
+        entinen = 1;
+        valo = 1;
+        //k_sem_give(&sem_yellow); // Yellow
     }
-}
+}}
 
 // Task2 function (Blue)
 void task2(void *, void *, void*) {
+    if (valo == 1) {   
+
     while (true) {
-        k_sem_take(&sem_yellow, K_FOREVER);
+        //k_sem_take(&sem_yellow, K_FOREVER);
         // Yellow: red + green ON, blue OFF
         gpio_pin_set_dt(&red, 1);
         //gpio_pin_set_dt(&blue, 0);
@@ -79,14 +90,20 @@ void task2(void *, void *, void*) {
         gpio_pin_set_dt(&green, 0);
         printk("Yellow off\n");
         k_msleep(500);
-        k_sem_give(&sem_green); // Next: green
+        entinen = 2;
+        valo = 2;
+        
+        //k_sem_give(&sem_green); // Green
     }
-}
+}}
 
 // Task3 function (Green)
 void task3(void *, void *, void*) {
+    if (valo == 2) {   
+
     while (true) {
-        k_sem_take(&sem_green, K_FOREVER);
+    
+        //k_sem_take(&sem_green, K_FOREVER);
         gpio_pin_set_dt(&red, 0);
         //gpio_pin_set_dt(&blue, 0);
         gpio_pin_set_dt(&green, 1);
@@ -95,6 +112,8 @@ void task3(void *, void *, void*) {
         gpio_pin_set_dt(&green, 0);
         printk("Green off\n");
         k_msleep(500);
-        k_sem_give(&sem_red); // Next: red
+        entinen = 0;
+        valo = 0;
+        //k_sem_give(&sem_red); // Red
     }
-}
+}}
