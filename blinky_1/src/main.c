d41a5856414e7d386224f013a92106e0cadfadab
Apotatoflew <76245607+Apotatoflew@users.noreply.github.com>
2025-09-16 23:00:39 +0300

    Uartilla toimivat valot

--- blinky_1/src/main.c
+++ blinky_1/src/main.c
@@ -1,119 +1,143 @@
-// *******************************
-// This example declares two tasks
-// and runs them in parallel
-
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+#include <string.h>
+#include <stdlib.h>
 
+/****************************
+ * Add to prj.conf:
+ * CONFIG_HEAP_MEM_POOL_SIZE=1024
+ ****************************/
 
-//Semaphores testi
+#define STACKSIZE 500
+#define PRIORITY 5
 
-//struct k_sem sem_red;
-//struct k_sem sem_green;
-//struct k_sem sem_yellow;
+// UART
+#define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
 
-int valo = 0;
-int entinen = 0;
-bool seis = false;
+// FIFO
+K_FIFO_DEFINE(dispatcher_fifo);
 
+struct data_t {
+    void *fifo_reserved;
+    char msg[20];
+};
 
-void task1(void *, void *, void*);
-void task2(void *, void *, void*);
-void task3(void *, void *, void*);
-
-#define	STACKSIZE	500
-#define	PRIORITY	5
-K_THREAD_DEFINE(tid1,STACKSIZE,task1,NULL,NULL,NULL,PRIORITY,0,0);
-K_THREAD_DEFINE(tid2,STACKSIZE,task2,NULL,NULL,NULL,PRIORITY,0,0);
-K_THREAD_DEFINE(tid3,STACKSIZE,task3,NULL,NULL,NULL,PRIORITY,0,0);
-
-
-static const struct gpio_dt_spec red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-//static const struct gpio_dt_spec blue = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+// LEDs
+static const struct gpio_dt_spec red   = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct gpio_dt_spec green = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 
-
-int init_led() {
-    int ret = gpio_pin_configure_dt(&red, GPIO_OUTPUT_ACTIVE);
-    //int ret2 = gpio_pin_configure_dt(&blue, GPIO_OUTPUT_ACTIVE);
-    int ret3 = gpio_pin_configure_dt(&green, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0 ||  ret3 < 0) {
-        printk("Error: Led configure failed\n");
+int init_leds(void) {
+    if (!device_is_ready(red.port) || !device_is_ready(green.port)) {
+        printk("Error: LED device not ready\n");
         return -1;
+    }
+    if (gpio_pin_configure_dt(&red, GPIO_OUTPUT_INACTIVE) < 0) return -1;
+    if (gpio_pin_configure_dt(&green, GPIO_OUTPUT_INACTIVE) < 0) return -1;
+    return 0;
+}
+
+int init_uart(void) {
+    if (!device_is_ready(uart_dev)) {
+        return 1;
     }
     return 0;
 }
 
 int main(void) {
-    init_led();
-    printk("LEDs initialized\n");
-    //k_sem_init(&sem_red, 1, 1);    // Start with red allowed
-    //k_sem_init(&sem_yellow, 0, 1);   // Blue waits
-    //k_sem_init(&sem_green, 0, 1);  // Green waits
+    if (init_uart() != 0) {
+        printk("UART initialization failed!\n");
+        return -1;
+    }
+    if (init_leds() != 0) {
+        printk("LED init failed!\n");
+        return -1;
+    }
+    printk("System ready\n");
     return 0;
 }
 
-// Task1 function (Red)
-void task1(void *, void *, void*) {
-if (valo == 0) {   
-    while (true) {
-        //k_sem_take(&sem_red, K_FOREVER);
-        gpio_pin_set_dt(&red, 1);
-        //gpio_pin_set_dt(&blue, 0);
-        gpio_pin_set_dt(&green, 0);
-        printk("Red on\n");
-        k_msleep(1000);
-        gpio_pin_set_dt(&red, 0);
-        printk("Red off\n");
-        k_msleep(500);
-        entinen = 1;
-        valo = 1;
-        //k_sem_give(&sem_yellow); // Yellow
-    }
-}}
-
-// Task2 function (Blue)
-void task2(void *, void *, void*) {
-    if (valo == 1) {   
+/********************
+ * UART task
+ */
+static void uart_task(void *unused1, void *unused2, void *unused3)
+{
+    char rc = 0;
+    char uart_msg[20];
+    memset(uart_msg, 0, 20);
+    int uart_msg_cnt = 0;
 
     while (true) {
-        //k_sem_take(&sem_yellow, K_FOREVER);
-        // Yellow: red + green ON, blue OFF
-        gpio_pin_set_dt(&red, 1);
-        //gpio_pin_set_dt(&blue, 0);
-        gpio_pin_set_dt(&green, 1);
-        printk("Yellow on\n");
-        k_msleep(1000);
-        gpio_pin_set_dt(&red, 0);
-        gpio_pin_set_dt(&green, 0);
-        printk("Yellow off\n");
-        k_msleep(500);
-        entinen = 2;
-        valo = 2;
-        
-        //k_sem_give(&sem_green); // Green
+        if (uart_poll_in(uart_dev, &rc) == 0) {
+         if (rc != '\r' && rc != '\n') {
+          uart_msg[uart_msg_cnt++] = rc;
+         } else {
+           if (uart_msg_cnt > 0) {   // Only send if buffer not empty
+           struct data_t *buf = k_malloc(sizeof(struct data_t));
+        if (buf == NULL) {
+            printk("Malloc failed!\n");
+            return;
+        }
+        snprintf(buf->msg, sizeof(buf->msg), "%s", uart_msg);
+        k_fifo_put(&dispatcher_fifo, buf);
+        printk("UART msg queued: %s\n", uart_msg);
+
+        // Reset buffer
+        uart_msg_cnt = 0;
+        memset(uart_msg, 0, sizeof(uart_msg));
+    }}
+}
+        k_msleep(10);
     }
-}}
+}
 
-// Task3 function (Green)
-void task3(void *, void *, void*) {
-    if (valo == 2) {   
-
+/********************
+ * Dispatcher task
+ */
+static void dispatcher_task(void *unused1, void *unused2, void *unused3)
+{
     while (true) {
-    
-        //k_sem_take(&sem_green, K_FOREVER);
+        struct data_t *rec_item = k_fifo_get(&dispatcher_fifo, K_FOREVER);
+        char sequence[20];
+        memcpy(sequence, rec_item->msg, sizeof(sequence));
+        k_free(rec_item);
+
+        printk("Dispatcher got: %s\n", sequence);
+
+        // Parse: R1000, Y500, G2000
+        char color = sequence[0];
+        int time = atoi(sequence + 1);   // read number after letter
+        printk("Parsed -> Color:%c Time:%dms\n", color, time);
+
         gpio_pin_set_dt(&red, 0);
-        //gpio_pin_set_dt(&blue, 0);
-        gpio_pin_set_dt(&green, 1);
-        printk("Green on\n");
-        k_msleep(1000);
         gpio_pin_set_dt(&green, 0);
-        printk("Green off\n");
-        k_msleep(500);
-        entinen = 0;
-        valo = 0;
-        //k_sem_give(&sem_red); // Red
+
+        if (color == 'R') {
+            gpio_pin_set_dt(&red, 1);
+            printk("RED ON\n");
+            k_msleep(time);
+            gpio_pin_set_dt(&red, 0);
+            printk("RED OFF\n");
+        } else if (color == 'Y') {
+            gpio_pin_set_dt(&red, 1);
+            gpio_pin_set_dt(&green, 1);
+            printk("YELLOW ON\n");
+            k_msleep(time);
+            gpio_pin_set_dt(&red, 0);
+            gpio_pin_set_dt(&green, 0);
+            printk("YELLOW OFF\n");
+        } else if (color == 'G') {
+            gpio_pin_set_dt(&green, 1);
+            printk("GREEN ON\n");
+            k_msleep(time);
+            gpio_pin_set_dt(&green, 0);
+            printk("GREEN OFF\n");
+        }
     }
-}}
+}
+
+K_THREAD_DEFINE(uart_thread, STACKSIZE, uart_task, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(dis_thread,  STACKSIZE, dispatcher_task, NULL, NULL, NULL, PRIORITY, 0, 0);
